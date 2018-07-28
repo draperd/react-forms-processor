7964e1c3ba36121cfb42776487cffdd315899450
Dave Draper <dave.draper20@googlemail.com>
2018-07-28 20:40:52 +1000

    Added basic react-beautiful-dnd to experiment

--- packages/examples/src/Repeats.js
+++ packages/examples/src/Repeats.js
@@ -1,7 +1,14 @@
 // @flow
 import React, { Component, type Node } from "react";
+import uniqueId from "lodash/uniqueId";
 import { Form, FormContext } from "react-forms-processor";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import type { FieldDef, OnFormChange } from "../../../types";
+
+type Item = {
+  id: string,
+  data: Object
+};
 
 type Props = {
   defaultValue: Object[], // TODO: Could be anything - not just fields
@@ -10,8 +17,9 @@ type Props = {
 };
 
 type State = {
-  items: Object[], // TODO: Could be anything - not just fields
-  forms: Node[]
+  value: Object[], // TODO: Could be anything - not just fields
+  //   forms: Node[],
+  items: Item[]
 };
 
 type CreateFormChangeHandler = (index: number) => OnFormChange;
@@ -20,9 +28,8 @@ const createFormForItem = (
   item: Object,
   targetIndex: number,
   fieldsForForm: FieldDef[],
-  createFormChangeHandler: CreateFormChangeHandler
+  formChangeHandler: OnFormChange
 ): Node => {
-  const formChangeHandler = createFormChangeHandler(targetIndex);
   return (
     <FormContext.Consumer>
       {context => {
@@ -43,32 +50,69 @@ const createFormForItem = (
   );
 };
 
-const addFormForItem = (
-  item: Object,
-  forms: Node[],
-  fieldsForForm: FieldDef[],
-  createFormChangeHandler: CreateFormChangeHandler
-): Node[] => {
-  const targetIndex = forms.length;
-  const newForm = createFormForItem(
-    item,
-    targetIndex,
-    fieldsForForm,
-    createFormChangeHandler
-  );
-  return [...forms, newForm];
+// const addFormForItem = (
+//   item: Object,
+//   forms: Node[],
+//   fieldsForForm: FieldDef[],
+//   createFormChangeHandler: CreateFormChangeHandler
+// ): Node[] => {
+//   const targetIndex = forms.length;
+//   const newForm = createFormForItem(
+//     item,
+//     targetIndex,
+//     fieldsForForm,
+//     createFormChangeHandler
+//   );
+//   return [...forms, newForm];
+// };
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
 };
+
+const grid = 8;
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250
+});
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "white",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
 
 export default class Repeats extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    const { fields, defaultValue: items = [], onChange } = props;
+    const { fields, defaultValue = [], onChange } = props;
 
-    const createFormChangeHandler = (targetIndex: number): OnFormChange => (
-      value,
-      isValid
-    ) => {
+    // Map the supplied array to an Item[] in order to give each piece of data an id for drag-and-drop
+    const items: Item[] = defaultValue.map(data => ({ id: uniqueId(), data }));
+    this.state = {
+      value: defaultValue,
+      items
+    };
+
+    // For each item, render a form
+  }
+
+  createFormChangeHandler(targetIndex: number): OnFormChange {
+    return (value, isValid) => {
       const { items } = this.state;
       items[targetIndex] = value;
       this.setState(
@@ -82,27 +126,76 @@ export default class Repeats extends Component<Props, State> {
         }
       );
     };
-
-    let forms = [];
-    items.forEach((item, index) => {
-      forms = addFormForItem(item, forms, fields, createFormChangeHandler);
-    });
-
-    this.state = {
-      forms,
-      items
-    };
-
-    // For each item, render a form
   }
 
   // TODO: Remove an item from state
   // TODO: Add an item to state
   // TODO: Re-order items in state
 
+  onDragEnd(result: any) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  }
+
+  getForms() {
+    const { items } = this.state;
+    const { fields, onChange } = this.props;
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd.bind(this)}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {items.map((item, index) => {
+                const formChangeHandler = this.createFormChangeHandler(index);
+                const form = createFormForItem(
+                  item,
+                  index,
+                  fields,
+                  formChangeHandler
+                );
+                return (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        {form}
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }
+
   render() {
     // Render the items in the state
-    const { forms } = this.state;
-    return <div>{forms}</div>;
+    return <div>{this.getForms()}</div>;
   }
 }
